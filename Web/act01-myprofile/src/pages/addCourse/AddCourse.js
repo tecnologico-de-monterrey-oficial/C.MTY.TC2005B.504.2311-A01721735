405d92804ce926553db70a7830b51ad454f9ff7b
Tellotello <a01721735@itesm.mx>
2023-04-20 15:22:23 -0600

    Corregir errores classes y addcourse

--- Web/act01-myprofile/src/pages/addCourse/AddCourse.js
+++ Web/act01-myprofile/src/pages/addCourse/AddCourse.js
@@ -1,223 +1,141 @@
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
-import {useState} from 'react';
-import Modal from 'react-bootstrap/Modal';
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import { useState } from "react";
 
-function AddCourse(show, onHide, handleAddCourse){
+function AddCourse({show, onhide, handleAddCourse}) {
+  const [id, setID] = useState(0);
+  const [name, setName] = useState("");
+  const [teacher, setTeacher] = useState("");
+  const [classroom, setClassroom] = useState("");
+  // const [course, setCourse] = useState({
+  //   id: 0,
+  //   name: "",
+  //   teacher: "",
+  //   classroom: ""
+  // });
 
-    const [id,setId] = useState('');
-    const [name, setName] = useState('');
-    const [teacher, setTeacher] = useState('');
-    const [classroom, setClassroom] = useState('');
-    const [course, setCourse] = useState({
-        id: 0,
-        name: '',
-        teacher: '',
-        classroom: ''
-    })
+  const handleChangeName = (event) => {
+    setName(event.target.value);
+    // setCourse( (prevCourse) => {
+    //   return {
+    //     ...prevCourse,
+    //     name: event.target.value
+    //     }
+    //   })
+    // console.log(event.target.value);
+  };
+  const handleChangeID = (event) => {
+    setID(event.target.value);
+    // setCourse( (prevCourse) => {
+    //   return {
+    //     ...prevCourse,
+    //     id: event.target.value
+    //     }
+    //   })
+    // console.log(event.target.value);
+  };
+  const handleChangeTeacher = (event) => {
+    setTeacher(event.target.value);
+    // setCourse( (prevCourse) => {
+    //   return {
+    //     ...prevCourse,
+    //     teacher: event.target.value
+    //     }
+    //   })
+    // console.log(event.target.value);
+  };
+  const handleChangeClassroom = (event) => {
+    setClassroom(event.target.value);
+    // setCourse( (prevCourse) => {
+    //   return {
+    //     ...prevCourse,
+    //     classroom: event.target.value
+    //     }
+    //   })
+    // console.log(event.target.value);
+  };
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const newCourse = {
+      id: id,
+      name: name,
+      teacher: teacher,
+      classroom: classroom
+    };
+    // setCourse(newCourse);
+    
+    handleAddCourse(newCourse);
 
-    const handleChangeName = (event) => {
+    setName("");
+    setID(0);
+    setTeacher("");
+    setClassroom("");
+    onhide();
+  };
 
-        setName (event.target.value);
-        setCourse (prevCourse => {
-            
-            return {...prevCourse, name: event.target.value}
-            })
-        //console.log(event.target.value);
-    }
-
-    const handleChangeId = (event) => {
-        setId (event.target.value);
-        setCourse(prevCourse => {
-            return {...prevCourse, id: event.target.value}
-            })
-        //console.log(event.target.value);
-    }
-
-    const handleChangeTeacher = (event) => {
-        setCourse(prevCourse => {
-            return {...prevCourse, teacher: event.target.value}
-            })
-        setTeacher (event.target.value);
-       // console.log(event.target.value);
-    }
-
-    const handleChangeClassroom = (event) => {
-        setClassroom (event.target.value);
-        setCourse(prevCourse => {   
-            return {...prevCourse, classroom: event.target.value}
-            })
-        //console.log(event.target.value);
-    }
-
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        console.log("Id: " + id);
-        console.log("Name: " + name);
-        console.log("Teacher: " + teacher);
-        console.log("Classroom: " + classroom);
-
-        const newCourse = {
-            id: id,
-            name: name,
-            teacher: teacher,
-            classroom: classroom
-        }
-
-        handleAddCourse(course);
-        setCourse(newCourse);
-        console.log(course);
-        onHide();
-        
-    }
-
-    return (
-
-        <Modal
-      show = {show}
-      onhide = {onHide}
+  return (
+    <Modal
+      show={show}
+      onHide={onhide}
       size="lg"
       aria-labelledby="contained-modal-title-vcenter"
       centered
     >
       <Modal.Header closeButton>
         <Modal.Title id="contained-modal-title-vcenter">
-        Agregar un nuevo curso
+          Agregar un nuevo curso
         </Modal.Title>
       </Modal.Header>
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-      
-    
-
-        <Form onSubmit={handleSubmit}>
-        <Modal.Body>
-  <h4>Centered Modal</h4>
-  <p>
-    Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-    dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
-    consectetur ac, vestibulum at eros.
-  </p>
-</Modal.Body>
-        <Form.Group className="mb-3" controlId="id">
-          <Form.Label>Id</Form.Label>
-            <Form.Control 
-            type="text" 
-            placeholder="Teclea el id" 
-            value = {id}
-            onChange = {handleChangeId}/>
-            
-          <Form.Text className="text-muted">
-          </Form.Text>
-        </Form.Group>
-
-        <Form.Group className="mb-3" controlId="name">
-        <Form.Label>Name</Form.Label>
-        <Form.Control 
-        type="text" 
-        placeholder="Teclea el nombre de la clase" 
-        value = {name}
-        onChange = {handleChangeName}/>
-    
-  <Form.Text className="text-muted">
-  </Form.Text>
-</Form.Group>
-        <Form.Group className="mb-3" controlId="Profesor">
-  <Form.Label>Profesor</Form.Label>
-    <Form.Control 
-    type="text" 
-    placeholder="Teclea el profesor" 
-    value = {teacher}
-    onChange = {handleChangeTeacher}/>
-    
-  <Form.Text className="text-muted">
-  </Form.Text>
-</Form.Group>
-        <Form.Group className="mb-3" controlId="Salon">
-  <Form.Label>Salon</Form.Label>
-    <Form.Control 
-    type="text" 
-    placeholder="Teclea el salon" 
-    value = {classroom}
-    onChange = {handleChangeClassroom}/>
-    
-  <Form.Text className="text-muted">
-  </Form.Text>
-</Form.Group>
-       
-<Modal.Footer>
-<Button onClick={onHide}>Close</Button>
-        <Button variant="primary" type="submit">
-          Submit
-        </Button>
-       
-    
-    </Modal.Footer>
-      </Form>
-    
-      </Modal>
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    );
+    <Form onSubmit={handleSubmit}>
+    <Modal.Body>
+      <Form.Group className="mb-3" controlId="id">
+        <Form.Label>ID</Form.Label>
+        <Form.Control
+          type="text"
+          placeholder="Teclea el ID"
+          value={id}
+          onChange={handleChangeID}
+        />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="name">
+        <Form.Label>Nombre</Form.Label>
+        <Form.Control
+          type="text"
+          placeholder="Teclea tu nombre"
+          value={name}
+          onChange={handleChangeName}
+        />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="teacher">
+        <Form.Label>Profesor</Form.Label>
+        <Form.Control
+          type="text"
+          placeholder="Teclea el nombre del profesor"
+          value={teacher}
+          onChange={handleChangeTeacher}
+        />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="classroom">
+        <Form.Label>Salón</Form.Label>
+        <Form.Control
+          type="text"
+          placeholder="Teclea el salón de la clase"
+          value={classroom}
+          onChange={handleChangeClassroom}
+        />
+      </Form.Group>
+      <Modal.Footer>
+      <Button onClick={onhide}>Close</Button>
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+      </Modal.Footer>
+    </Modal.Body>
+    </Form>
+    </Modal>
+  );
 }
 
-export default AddCourse
+export default AddCourse;
--- Web/act01-myprofile/src/pages/classes/Classes.js
+++ Web/act01-myprofile/src/pages/classes/Classes.js
@@ -1,73 +1,68 @@
-import { Container, Row, Col, Button} from 'react-bootstrap';
-import classesData from '../../data/classesData.js'
-import {useState} from 'react';
-import AddCourse from '../addCourse/AddCourse.js';
+import { Container, Row, Col, Button } from 'react-bootstrap';
+import classesData from '../../data/classesData';
+import { useState } from 'react';
+import AddCourse from '../addCourse/AddCourse';
+
+function Classes() {
+
+    const [classes, setClasses] = useState(classesData);
+    const [modalShow, setModalShow] = useState(false);
 
 
-function Classes(){
+    const handleDelete = (id) => {
+        console.log('Hola -' + id);
+        setClasses( (prevClasses) => {
+            return prevClasses.filter((course) => course.id !== id);
+        });
+    }
 
-const [classes, setClasses] = useState(classesData);
-const [modalShow, setModalShow] = useState(false);
+    const addCourse = (course) => {
+        console.log('Hola Crayola');
+        console.log(course);
+        setClasses( (prevClasses) => {
+            return [
+                ...prevClasses, 
+                course
+            ];
+        });
+    }
 
-const handleDelete = (id) => {
-    console.log(id);
-    setClasses( (prevClasses) => {
-        return prevClasses.filter((course) => course.id !== id);
-    })
-    
-    
-}
+    const handleShow = () => {
+        setModalShow(true);
+    }
 
-const addCourse = (course) => {
-    console.log("Course");
-    setClasses( (prevClasses) => {
-        return [...prevClasses, course];
-    });
-}
-    
-const handleShow = () => {
-  setModalShow(true);
+    const handleClose = () => {
+        setModalShow(false);
+    }
 
-}
-
-const handleClose = () => {
-  setModalShow(false);
-}
-
-    return(
-        <>  
-    
-            
-    
-            <Container>
-      <Row>
-        <Col>Course</Col>
-        <Col>Teacher</Col>
-        <Col>Classroom</Col>
-        <Col><Button variant = "success" onClick = {handleShow}>Agregar</Button></Col>
-      </Row>
-
-      {classes.length === 0 && 
+    return (
+        <>
+    <Container>
         <Row>
-            <Col>No hay clases</Col>
+            <Col>Clase</Col>
+            <Col>Profesor</Col>
+            <Col>Salón</Col>
+            <Col><Button variant="success" onClick={handleShow}>Agregar</Button></Col>
         </Row>
-      }
-      {classes.map((course) => (
-        <Row key = {course.id}>
-        <Col>{course.name}</Col>
-        <Col>{course.teacher}</Col>
-        <Col>{course.classroom}</Col>
-        <Col><Button variant = "danger" onClick = {() => {handleDelete(course.id)}}>Borrar</Button></Col>
-      </Row>
+        {classes.length === 0 &&
+            <Row>
+                <Col>No hay clases</Col>
+            </Row>
+        }
+        {classes.map((course) => (
+            <Row key={course.id}>
+                <Col>{course.name}</Col>
+                <Col>{course.teacher}</Col>
+                <Col>{course.classroom}</Col>
+                <Col><Button variant="danger" onClick={() => {handleDelete(course.id)}}>Borrar</Button></Col>
+            </Row>
         ))}
     </Container>
-    <AddCourse
-    show = {modalShow}
-    onHide = {handleClose}
-    handleAddCourse = {addCourse}
-    />
+    <AddCourse 
+    show={modalShow} 
+    onhide={handleClose} 
+    handleAddCourse={addCourse}/>
     </>
-   
     );
 }
 
